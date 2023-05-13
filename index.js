c1d1fdbad1d62760eae8b5a3433032fbce7ffe54
habib-N19 <habiburnabiarafat@gmail.com>
2023-05-13 12:32:19 +0600

    added an updated route api point for coffee update

--- index.js
+++ index.js
@@ -29,11 +29,40 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
+        // update operation, specific id based coffee
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.findOne(query)
+            res.send(result)
+        })
         // getting coffee data from client form
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body
             // console.log(newCoffee);
             const result = await coffeeCollection.insertOne(newCoffee)
+            res.send(result)
+        })
+
+        // update a specific coffee
+
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedCoffee = req.body
+            const coffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    quantity: updatedCoffee.quantity,
+                    supplier: updatedCoffee.supplier,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            }
+            const result = await coffeeCollection.updateOne(filter, coffee, options)
             res.send(result)
         })
         // deleting a specific 
